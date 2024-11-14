d854d2fa931884c4c29159835a14f74f43c03107
Kais Rafie <2371100@edu.vaniercollege.qc.ca>
2024-11-14 13:50:15 -0500

    whatever

--- src/main/java/patterns/dental/clinic/Main.java
+++ src/main/java/patterns/dental/clinic/Main.java
@@ -1,5 +1,10 @@
 package patterns.dental.clinic;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import patterns.dental.clinic.model.bill.*;
 import patterns.dental.clinic.model.user.Dentist;
 import patterns.dental.clinic.model.user.Patient;
@@ -11,8 +16,27 @@ import java.sql.Date;
 import java.sql.Time;
 import java.time.LocalDate;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                    "src/main/java/patterns/dental/clinic/pages/MainFrame.fxml"));
+            AnchorPane root = loader.load();
+
+            Scene scene = new Scene(root);
+
+
+            stage.setScene(scene);
+            stage.setTitle("JavaFX Application");
+
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
